66e3e3e12aacedc606b20c25487dfc278d1b4a66
sbergeroncp <93004917+sbergeroncp@users.noreply.github.com>
2024-04-26 13:35:04 -0400

    Mise à jour main.blocks, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -126,6 +126,25 @@ ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
 ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
 ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
 `;
+            case "image2":
+            case "spaceship_1":return img`
+. . . . . . 9 9 . . . . . . . . 
+. . . . . 9 1 1 9 . . . . . . . 
+. . . . 9 1 1 1 1 9 . . . . . . 
+. . . . 1 1 6 a 1 1 . . . . . . 
+. . . . 1 6 c c a 1 . . . . . . 
+. . . . 1 1 c c 1 1 . . . . . . 
+. . . 1 1 1 1 1 1 1 1 . . . . . 
+. . . 9 6 1 1 1 1 a 9 . . . . . 
+. . 9 6 f 1 1 1 1 f a 9 . . . . 
+. . 9 f f 1 1 1 1 f f 9 . . . . 
+. 9 6 f f 1 1 1 1 f f a 9 . . . 
+. 9 f f f 1 1 1 1 f f f 9 . . . 
+9 6 f f f 1 1 1 1 f f f a 9 . . 
+9 6 f . f f f f f f . f a 9 . . 
+d . . . . a a a a . . . . 9 . . 
+. . . . . 4 5 5 4 . . . . . . . 
+`;
         }
         return null;
     })
