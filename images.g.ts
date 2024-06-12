d3c685db6dec3f93c7f5ee3294e70595d6d2efa1
sbergeroncp <93004917+sbergeroncp@users.noreply.github.com>
2024-06-12 09:24:47 -0400

    Mise à jour images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -3,25 +3,6 @@ namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "image2":
-            case "spaceship_1":return img`
-. . . . . . 9 9 . . . . . . . . 
-. . . . . 9 1 1 9 . . . . . . . 
-. . . . 9 1 1 1 1 9 . . . . . . 
-. . . . 1 1 6 a 1 1 . . . . . . 
-. . . . 1 6 c c a 1 . . . . . . 
-. . . . 1 1 c c 1 1 . . . . . . 
-. . . 1 1 1 1 1 1 1 1 . . . . . 
-. . . 9 6 1 1 1 1 a 9 . . . . . 
-. . 9 6 f 1 1 1 1 f a 9 . . . . 
-. . 9 f f 1 1 1 1 f f 9 . . . . 
-. 9 6 f f 1 1 1 1 f f a 9 . . . 
-. 9 f f f 1 1 1 1 f f f 9 . . . 
-9 6 f f f 1 1 1 1 f f f a 9 . . 
-9 6 f . f f f f f f . f a 9 . . 
-d . . . . a a a a . . . . 9 . . 
-. . . . . 4 5 5 4 . . . . . . . 
-`;
             case "image3":
             case "ennemi_1":return img`
 . . . . . 2 2 . . . . . 
@@ -321,6 +302,25 @@ ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
 ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
 ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
 ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+`;
+            case "image2":
+            case "spaceship_1":return img`
+. . . . . . 9 9 . . . . . . . . 
+. . . . . 9 1 1 9 . . . . . . . 
+. . . . 9 1 1 1 1 9 . . . . . . 
+. . . . 9 1 6 a 1 9 . . . . . . 
+. . . . 9 6 c c a 9 . . . . . . 
+. . . . 9 1 c c 1 9 . . . . . . 
+. . . 9 1 1 1 1 1 9 9 . . . . . 
+. . . 9 6 1 1 1 1 a 9 . . . . . 
+. . 9 6 f 1 1 1 1 f a 9 . . . . 
+. . 9 f f 1 1 1 1 f f 9 . . . . 
+. 9 6 f f 1 1 1 1 f f a 9 . . . 
+. 9 f f f 1 1 1 1 f f f 9 . . . 
+9 6 f f f 9 a a 9 f f f a 9 . . 
+9 6 f . f a 5 5 a f . f a 9 . . 
+d . . . . 2 5 5 2 . . . . 9 . . 
+. . . . 2 4 . . 4 2 . . . . . . 
 `;
         }
         return null;
